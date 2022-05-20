3c11a5fae51ed09d7c70a1a810db0e554beed039
Juan Betancourt <juancbetancourt11@gmail.com>
2022-05-20 16:15:43 +0000

    Working code

--- main.py
+++ main.py
@@ -38,11 +38,10 @@ import random
 
 
 def deal_card():
-  """Returns a random card from the deck."""
-  cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-  card = random.choice(cards)
-  return card
-deal_card()
+    """Returns a random card from the deck."""
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
 
 #Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
 user_cards = []
@@ -58,8 +57,32 @@ for _ in range(2):
 #and returns the score. 
 #Look up the sum() function to help you do this.
 
-def calculate_score():
 
+def calculate_score(num1, num2):
+    print(user_cards)
+    total_user = sum(num1)
+    if total_user > 21:
+      user_cards.remove(11) 
+      user_cards.append(1)
+      total_user = sum(num1)
+    elif total_user == 21:
+      return "BlackJack"
+      print("BlackJack, you win")
+    print(total_user)
+  
+    print("\n")
+
+    print(computer_cards)
+    total_computer = sum(num2)
+    if total_user > 21:
+      computer_cards.remove(11) 
+      computer_cards.append(1)
+    elif total_computer == 21:
+      return "BlackJack"
+      print("BlackJack, the computer wins")
+    print(total_computer)
+
+calculate_score(num1 = user_cards, num2 = computer_cards)
 #Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
 
 #Hint 8: Inside calculate_score() check for an 11 (ace). If the score is already over 21, remove the 11 and replace it with a 1. You might need to look up append() and remove().
@@ -75,4 +98,3 @@ def calculate_score():
 #Hint 13: Create a function called compare() and pass in the user_score and computer_score. If the computer and user both have the same score, then it's a draw. If the computer has a blackjack (0), then the user loses. If the user has a blackjack (0), then the user wins. If the user_score is over 21, then the user loses. If the computer_score is over 21, then the computer loses. If none of the above, then the player with the highest score wins.
 
 #Hint 14: Ask the user if they want to restart the game. If they answer yes, clear the console and start a new game of blackjack and show the logo from art.py.
-
